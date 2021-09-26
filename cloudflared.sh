76c87ffeb93607447848fe5ec101e83971581cbc
YXBT <86468319+YXBT@users.noreply.github.com>
2021-09-27 00:36:29 +0800

    Update cloudflared.sh

--- cloudflared.sh
+++ cloudflared.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://github.com/cloudflare/cloudflared/releases/download/2021.9.1/cloudflared-linux-amd64
 chmod +x cloudflared-linux-amd64
-nohup ./cloudflared-linux-amd64 tunnel > connect.txt
+nohup ./cloudflared-linux-amd64 tunnel > connect.txt &
 address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
 curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}"
