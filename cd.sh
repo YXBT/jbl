5a826cb28d0698dbc3f74fa5c460b565d4268ed6
YXBT <86468319+YXBT@users.noreply.github.com>
2021-09-27 18:35:35 +0800

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -5,3 +5,4 @@ chmod +x cloudflared-linux-amd64
 nohup ./cloudflared-linux-amd64 tunnel > connect.txt &
 address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
 curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}"
+./jbl -config=https://raw.githubusercontent.com/yxbt/v2/main/eu-de.json
