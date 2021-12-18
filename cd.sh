c1a635eba892aaa1b81b727ac1a2a35f6ef99516
YXBT <86468319+YXBT@users.noreply.github.com>
2021-12-18 19:16:12 +0800

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /app/htdocs/
-wget https://github.com/cloudflare/cloudflared/releases/download/2021.9.1/cloudflared-linux-amd64
-chmod +x cloudflared-linux-amd64
-nohup ./cloudflared-linux-amd64 tunnel > connect.txt 2>&1 &
-sleep 20
-address=$(cat /app/htdocs/connect.txt | grep trycloudflare | grep https | awk '{print $4}')
-curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}"
+#wget https://github.com/cloudflare/cloudflared/releases/download/2021.9.1/cloudflared-linux-amd64
+#chmod +x cloudflared-linux-amd64
+#nohup ./cloudflared-linux-amd64 tunnel > connect.txt 2>&1 &
+#sleep 20
+#address=$(cat /app/htdocs/connect.txt | grep trycloudflare | grep https | awk '{print $4}')
+#curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}"
 ./jbl -config=https://raw.githubusercontent.com/yxbt/v2/main/eu-de.json
